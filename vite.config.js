62ed2a08c134cf2f4bfb6056d44a12eb890a99ac
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2023-03-09 02:42:18 +0700

    Added Bootstrap, React Router, and Axios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: path.resolve((__dirname, "src")),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve((__dirname, "node_modules/bootstrap")),
+    },
+  },
+});
