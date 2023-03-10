295400867e1b3792ccb2aa53bfb740de9aa6fa33
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2023-03-10 17:17:14 +0700

    Updated npm version

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import store from "./store/store";
 
 import App from "./components/App";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
