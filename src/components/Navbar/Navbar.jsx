e6c9295fd6d68c2d64d4e73c38fb753b6de769d4
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2023-04-09 15:23:32 +0700

    Updated new layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,33 +3,33 @@ import './Navbar.css';
 import { BrowserRouter, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="Navbar col-4">
-      <Nav vertical>
-        <NavItem className='NavItem'>
-          <NavLink href="/info">Thông tin cá nhân</NavLink>
-        </NavItem>
-        <NavItem className='NavItem'>
-          <NavLink href="/dashboard">Lịch biểu</NavLink>
-        </NavItem>
-        <NavItem className='NavItem'>
-          <NavLink href="/tasks">Ca và nhiệm vụ</NavLink>
-        </NavItem>
-        <NavItem className='NavItem'>
-          <NavLink href="/collecting-points">Điểm thu gom</NavLink>
-        </NavItem>
-        <NavItem className='NavItem'>
-          <NavLink href="/staffs">Nhân viên</NavLink>
-        </NavItem>
-        <NavItem className='NavItem'>
-          <NavLink href="/settings">Cài đặt</NavLink>
-        </NavItem>
-        <NavItem className='NavItem'>
-          <NavLink href="/logout">Đăng xuất</NavLink>
-        </NavItem>
-      </Nav>
-    </div>
-  );
+	return (
+		<div className="Navbar col-3">
+			<Nav vertical>
+				<NavItem className="NavItem">
+					<NavLink href="/info">Thông tin cá nhân</NavLink>
+				</NavItem>
+				<NavItem className="NavItem">
+					<NavLink href="/dashboard">Lịch biểu</NavLink>
+				</NavItem>
+				<NavItem className="NavItem">
+					<NavLink href="/tasks">Ca và nhiệm vụ</NavLink>
+				</NavItem>
+				<NavItem className="NavItem">
+					<NavLink href="/collecting-points">Điểm thu gom</NavLink>
+				</NavItem>
+				<NavItem className="NavItem">
+					<NavLink href="/staffs">Nhân viên</NavLink>
+				</NavItem>
+				<NavItem className="NavItem">
+					<NavLink href="/settings">Cài đặt</NavLink>
+				</NavItem>
+				<NavItem className="NavItem">
+					<NavLink href="/logout">Đăng xuất</NavLink>
+				</NavItem>
+			</Nav>
+		</div>
+	);
 }
 
 export default App;
