d470a8ad3e9ceba4445500a6901e551462bb4237
KhoiDinh <khoi.dinhiamkhoi@hcmut.edu.vn>
2023-04-12 20:07:47 +0700

    add shift page

---
+++ src/pages/SiftPage/shiftPage.jsx
@@ -0,0 +1,15 @@
+
+import classNames from "classnames/bind";
+import styles from "./shiftPage.module.scss"
+
+const cx = classNames.bind(styles)
+
+function SiftPage() {
+    return ( 
+        <div className={cx("container")}>
+            
+        </div>
+     );
+}
+
+export default SiftPage;
